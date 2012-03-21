a38feeb36ff7ecf8c8f0f4db596e61aa600b81ee
Francisco José Marques Vieira <francisco.j.m.vieira@gmail.com>
2012-03-21 04:12:54 +0000

    Add a setup.py and requirements.txt file

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+      name='coursera',
+      version='1.0',
+      description='Automatize tasks in Coursera course\'s sites, namely the download of resources.',
+      author='Francisco José Marques Vieira',
+      author_email='francisco.j.m.vieira@gmail.com',
+      url='https://github.com/fvieira/coursera_resources_downloader',
+      scripts = ['coursera'],
+      requires=[
+          'argparse',
+          'lxml',
+          'mechanize',
+      ],
+)
